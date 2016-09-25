4b1826ad1cc41488581e52a69e480539377cc330
ekas <ekaspreet93.singh@gmail.com>
2016-09-25 23:00:32 +0530

    Working with JS Basics

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,77 @@
+
+/*var awesomeThoughts = " I am Ekas Preet Singh and I am Awesome";
+var funThoughts = awesomeThoughts.replace("Awesome","Fun");
+console.log(funThoughts);
+$("#main").append(funThoughts);
+*/
+
+
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Ekas Preet Singh";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+*/
+
+
+
+/*
+var s = "audacity";
+
+var udacityizer = function(s) {  
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+    
+    s = s[1].toUpperCase() + s.slice(2);
+    return s;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
+*/
+
+/*
+var skills = ["programming","awesomeness","teaching","JS"];
+//$("#main").append(skills +",");
+//$("#main").append(skills[0]);
+$("#main").append(skills.length);
+console.log(skills);
+*/
+
+//var multiArray = ["Euler", 3.14159, {name: "James", job: "Course Developer"}, myFunc];
+
+/*
+var sampleArray = [0,0,7];
+
+var incrementLastArrayElement = function(_array) {
+    var newArray = [];
+    newArray = _array.slice(0);
+    var lastElement = newArray.pop();
+    newArray.push(lastElement+1);
+    return newArray;
+};
+
+// Did your code work? The line below will tell you!
+console.log(incrementLastArrayElement(sampleArray));
+*/
+
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    var nameSplit = oldName.split(" ");
+    nameSplit[0] = nameSplit[0].slice(0,1).toUpperCase() + nameSplit[0].slice(1).toLowerCase();
+    nameSplit[1] = nameSplit[1].toUpperCase();
+    finalName = nameSplit.join(" ");
+    
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
